356b556aca328210aa9c4afe2148f73772873e58
Omar Abdelati <42877467+Ammuri@users.noreply.github.com>
2025-06-22 14:48:18 +0000

    Level 1 commit

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,22 +12,36 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+MAXIMUM_ITEM_AMOUNT = 10_000
+MAX_ITEM_QUANTITY = 100
+MIN_ITEM_QUANTITY = 0
+MAXIMUM_PAYABLE_AMOUNT = 1_000_000
+
 def validorder(order: Order):
-    net = 0
+    payments = Decimal('0')
+    expenses = Decimal('0')
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if -MAXIMUM_ITEM_AMOUNT <= item.amount <= MAXIMUM_ITEM_AMOUNT:
+                payments += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if isinstance(item.quantity, int) and MIN_ITEM_QUANTITY < item.quantity <= MAX_ITEM_QUANTITY and \
+            0 < item.amount <= MAXIMUM_PAYABLE_AMOUNT:
+                expenses += Decimal(str(item.amount)) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    #Checking the total amount payable against the maximum payable amount
+    if abs(payments) > MAXIMUM_PAYABLE_AMOUNT or abs(expenses) > MAXIMUM_PAYABLE_AMOUNT:
+        return "Total amount payable for an order exceeded"
+
+    if payments != expenses:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, payments - expenses)
     else:
         return "Order ID: %s - Full payment received!" % order.id
